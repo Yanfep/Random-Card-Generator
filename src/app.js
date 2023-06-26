5a03aa2b2bb32e01f2a9cff35445b858ad25fd5f
Yanfe Pedroza Alberto <38539546+Yanfep@users.noreply.github.com>
2023-06-26 18:09:08 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -7,5 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Numero");
+};
+
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
 };
